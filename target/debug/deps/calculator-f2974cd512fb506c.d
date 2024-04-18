adf89426f29753959804a4345cc21203516b9e67
Tazbidus Samad Tazbid <98827508+tstazbid@users.noreply.github.com>
2024-04-18 19:28:38 +0600

    initial

---
+++ target/debug/deps/calculator-f2974cd512fb506c.d
@@ -0,0 +1,5 @@
+C:\Rust\calculator\target\debug\deps\libcalculator-f2974cd512fb506c.rmeta: src\main.rs
+
+C:\Rust\calculator\target\debug\deps\calculator-f2974cd512fb506c.d: src\main.rs
+
+src\main.rs:
